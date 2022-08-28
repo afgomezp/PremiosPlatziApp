e5a29f531e39d00007e50771ff326b8cc5c5e642
Felipe Gomez <afgomezp@gmail.com>
2022-08-28 15:42:46 -0500

    version del proyecto antes de tener formularios en las vista detail

--- premiosplatziapp/polls/views.py
+++ premiosplatziapp/polls/views.py
@@ -1,8 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse
+
+from .models import Question
 
 # Create your views here.
 
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse('hello world')
+    latest_question_list = Question.objects.all()
+
+    return render(request, "polls/index.html", {
+        "latest_question_list":latest_question_list
+    } )
+
+
+
+def detail(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, "polls/detail.html", {
+        "question": question
+    })
+
+def results(request, question_id):
+    return HttpResponse (f'estas viendo los resultados a la pregunta número: {question_id}')
+
+def vote(request, question_id):
+    return HttpResponse (f'estas votando a la pregunta número: {question_id}')
