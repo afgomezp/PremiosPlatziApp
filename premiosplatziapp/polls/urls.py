96a19dfe549579831cd3f6dc5f10d16c08c60234
Felipe Gomez <afgomezp@gmail.com>
2022-08-30 21:17:27 -0500

    se hizo la primera clase Indexview

--- premiosplatziapp/polls/urls.py
+++ premiosplatziapp/polls/urls.py
@@ -4,7 +4,7 @@ from . import views
 app_name = "polls"
 urlpatterns = [
     #ex: /polls/
-    path("", views.index, name="index"),
+    path("", views.IndexView.as_view(), name="index"),
 
     #ex details: /polls/5
     path("<int:question_id>/felipe", views.detail, name="detail"),
@@ -15,4 +15,3 @@ urlpatterns = [
     #ex vote: /polls/5/vote
     path("<int:question_id>/vote/", views.vote, name="vote")
 ]
-
--- premiosplatziapp/polls/views.py
+++ premiosplatziapp/polls/views.py
@@ -3,17 +3,25 @@ from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse, HttpResponseRedirect
 from django.urls import reverse
 from .models import Question, Choice
+from django.views import generic
 
 # Create your views here.
 
 from django.http import HttpResponse
 
-def index(request):
+"""def index(request):
     latest_question_list = Question.objects.all()
 
     return render(request, "polls/index.html", {
         "latest_question_list":latest_question_list
-    } )
+    } )"""
+
+class IndexView(generic.ListView):
+    template_name = "polls/index.html"
+    context_object_name = "latest_question_list"
+
+    def get_queryset(self):
+        return Question.objects.order_by("-pub_date")[0:5]
 
 
 def detail(request, question_id):
