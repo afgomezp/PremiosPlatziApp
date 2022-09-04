1ebb47c788ba6db2f3306e8806556693cbd6829a
Felipe Gomez <afgomezp@gmail.com>
2022-09-03 23:05:32 -0500

    version con 7 test creados

--- premiosplatziapp/polls/tests.py
+++ premiosplatziapp/polls/tests.py
@@ -1,8 +1,10 @@
 import datetime
+from urllib import response
 
 from django.test import TestCase
 from django.utils import timezone
 from .models import Question
+from django.urls.base import reverse
 
 # Create your tests here.
 
@@ -25,5 +27,32 @@ class QuestionModelTest(TestCase):
         time = timezone.now() - datetime.timedelta(hours=23, minutes=59)
         self.question.pub_date = time
         self.assertIs(self.question.was_published_recently(), True)
+    
+def create_question(question_text, days):
+    time = timezone.now() + datetime.timedelta(days=days)
+    return Question.objects.create(question_text=question_text, pub_date=time)
 
-
+class QuestionIndexViewtest(TestCase):
+        
+    def test_no_question(self):
+        response= self.client.get(reverse('polls:index'))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "No polls are available.")
+        self.assertQuerysetEqual(response.context["latest_question_list"], [])
+    
+    def test_no_future_question_displayed(self):
+        response= self.client.get(reverse('polls:index'))
+        time = timezone.now() + datetime.timedelta(days=30)
+        future_question = Question(question_text='Qué es lo que más te gusta de platzi', pub_date=time)
+        self.assertNotIn(future_question, response.context["latest_question_list"])
+    
+    def test_future_question(self):
+        create_question("soy pregunta del futuo", days=30)
+        response= self.client.get(reverse('polls:index'))
+        self.assertContains(response, "No polls are available.")
+        self.assertQuerysetEqual(response.context["latest_question_list"], [])
+    
+    def test_past_question(self):
+        question = create_question("soy pregunta del pasado", days=-30)
+        response= self.client.get(reverse('polls:index'))
+        self.assertQuerysetEqual(response.context["latest_question_list"], [question])
