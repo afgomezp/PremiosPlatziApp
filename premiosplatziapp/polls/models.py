11afa97d01cdd378335394f5f6b1c3d76e8ca5c5
Felipe Gomez <afgomezp@gmail.com>
2022-09-03 19:27:58 -0500

    version con los test al modelo Question

--- premiosplatziapp/polls/models.py
+++ premiosplatziapp/polls/models.py
@@ -1,5 +1,8 @@
 from django.db import models
 
+from django.utils import timezone
+import datetime
+
 # Create your models here.
 
 class Question(models.Model):
@@ -10,7 +13,7 @@ class Question(models.Model):
         return self.question_text
     
     def was_published_recently(self):
-        return self.pub_date >= timezone.now() -datetime.timedelta(days=1)
+        return timezone.now() >= self.pub_date >= timezone.now() - datetime.timedelta(days=1)
 
 
 
--- premiosplatziapp/polls/tests.py
+++ premiosplatziapp/polls/tests.py
@@ -1,3 +1,29 @@
+import datetime
+
 from django.test import TestCase
+from django.utils import timezone
+from .models import Question
 
 # Create your tests here.
+
+class QuestionModelTest(TestCase):
+
+    def setUp(self):
+        self.question = Question(question_text = '¿Quién es el mejor course director de platzi?')
+
+    def test_was_published_recently_with_future_question(self):
+        time = timezone.now() + datetime.timedelta(days=30)
+        self.question.pub_date = time
+        self.assertIs(self.question.was_published_recently(), False)
+
+    def test_was_published_recently_with_past_question(self):
+        time = timezone.now() - datetime.timedelta(days=1, minutes=1)
+        self.question.pub_date = time
+        self.assertIs(self.question.was_published_recently(), False)
+
+    def test_was_published_recently_with_recent_question(self):
+        time = timezone.now() - datetime.timedelta(hours=23, minutes=59)
+        self.question.pub_date = time
+        self.assertIs(self.question.was_published_recently(), True)
+
+
