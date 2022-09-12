5cf0c7ef1110194e08d2960473ce1733f30edb62
Felipe Gomez <afgomezp@gmail.com>
2022-09-12 11:07:37 -0500

    se agregaron funcionalidades al admni de django

--- premiosplatziapp/polls/admin.py
+++ premiosplatziapp/polls/admin.py
@@ -6,38 +6,20 @@ from .models import Question, Choice
 from django import forms
 from django.forms.models import BaseInlineFormSet
 
-#para crear preguntas desde el administrador
-admin.site.register(Question)
 
-
-"""class AtLeastOneRequiredInlineFormSet(BaseInlineFormSet):
-
-    def clean(self):
-        #Check that at least one choice has been entered.
-        super(AtLeastOneRequiredInlineFormSet, self).clean()
-        if any(self.errors):
-            return
-        if not any(cleaned_data and not cleaned_data.get('DELETE', False)
-            for cleaned_data in self.cleaned_data):
-            raise forms.ValidationError('At least one choice required.')
-
-
-class ChoicesInline(admin.TabularInline):
+class ChoiceImline(admin.StackedInline):
     model = Choice
-    formset= AtLeastOneRequiredInlineFormSet
-    extra = 1
-    exclude= ['votes']
-
-class QuestionAdmin(admin.ModelAdmin):
-    inlines=(ChoicesInline,)
-
-    def save_formset(self, request, form, formset, change):
-        instances = formset.save(commit=False)
-        for obj in formset.deleted_objects:
-            obj.delete()        
-        for instance in instances:
-            instance.save()            
+    extra = 3
 
 
-admin.site.register(Question, QuestionAdmin)
-#admin.site.register(Choice)"""
+class Questionadmin(admin.ModelAdmin):
+    fields=["pub_date", "question_text"]
+    inlines=[ChoiceImline]
+    list_display = ("question_text", "pub_date","was_published_recently")
+    list_filter = ["pub_date"]
+    search_fields = ["question_text"]
+
+
+#para crear preguntas desde el administrador
+admin.site.register(Question, Questionadmin)
+
